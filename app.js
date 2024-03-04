828d4175cb5464a5679dddaef851d1d18d9e55aa
taruncodex <130213971+taruncodex@users.noreply.github.com>
2024-03-04 07:16:33 +0530

    Update app.js
    change homeepage name in app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("Public"));
 
 app.get("/" , function(req , res){
- res.sendFile(__dirname + "/HomePage.html")
+ res.sendFile(__dirname + "/index.html")
 
 });
  
